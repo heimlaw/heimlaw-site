e6579f9055e5faaf0c0ae4d5208f02e18a6e600c
heimlaw <alaskagreenlandantartica@gmail.com>
2026-09-21 23:52:12 +0900

    공식로고 적용 및 연락처 등록

--- assets/site-config.js
+++ assets/site-config.js
@@ -3,7 +3,7 @@
  * 값이 비어 있으면 해당 항목은 화면에서 자동으로 숨겨집니다.
  */
 window.HEIMLAW_CONFIG = {
-  phoneDisplay: "",
-  phoneHref: "",
-  consultationHours: ""
+  phoneDisplay: "070-8919-8319",
+  phoneHref: "02-6008-8119",
+  consultationHours: "평일 09:~18:00 | 사전 예약필수"
 };
